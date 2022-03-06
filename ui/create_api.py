a15dc060362caf79c7c1455167139f1758070035
Ashutosh Raman <61544363+ashutosh0709@users.noreply.github.com>
2022-03-06 19:37:10 +0530

    Update create_api.py

--- ui/create_api.py
+++ ui/create_api.py
@@ -32,6 +32,7 @@ os.chdir("../")
 src_path = os.getcwd()
 new_path = os.path.join(src_path ,'output')
 os.chdir(new_path)
+print(os.chdir())
 
 def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
     import boto3
@@ -54,7 +55,7 @@ bucketname = os.getenv('bucketname')
 downloadDirectoryFroms3(bucketname , "all_keyword_vectors") 
 
 os.chdir(curr_path)
-
+print(os.chdir())
 ##################################################################
 from one_time_load_files import one_load_files
 lemmatizer , stop_words = one_load_files()
